6c43e8157694b5643fac20255294e01677d9a553
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-02-16 18:29:23 -0500

    Remove unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:simple_todo/models/listmodel.dart';
 import 'package:simple_todo/views/listitemcard.dart';
 import 'package:provider/provider.dart';
