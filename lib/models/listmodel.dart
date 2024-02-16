eacc7087dffff0aa07d51c23d99696c624d0dee6
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-02-16 18:26:39 -0500

    Add methods for updating entry values

--- lib/models/listmodel.dart
+++ lib/models/listmodel.dart
@@ -9,12 +9,27 @@ class TodoListModel extends ChangeNotifier {
     return TodoListModel([]);
   }
 
+  void updateContents(int entryIndex, String newText) {
+    items[entryIndex].contents = newText;
+    notifyListeners();
+  }
+
+  void updateChecked(int entryIndex, bool newValue) {
+    items[entryIndex].completed = newValue;
+    notifyListeners();
+  }
+
   void swap(int oldIndex, int newIndex) {
     if (oldIndex < newIndex) {
       newIndex--;
     }
     TodoEntry entry = items.removeAt(oldIndex);
     items.insert(newIndex, entry);
+    notifyListeners();
+  }
+
+  void appendNew() {
+    items.add(TodoEntry(false, ""));
     notifyListeners();
   }
 }
