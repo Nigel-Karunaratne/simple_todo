f5e564b07ff9e5aa84eb84a2fb1131240327f548
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-02-16 22:15:54 -0500

    Add ability to convert to and from a JSON object

--- lib/models/listmodel.dart
+++ lib/models/listmodel.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class TodoListModel extends ChangeNotifier {
@@ -8,6 +9,15 @@ class TodoListModel extends ChangeNotifier {
   factory TodoListModel.empty() {
     return TodoListModel([]);
   }
+
+  //Gets most recent list in json file, or makes empty if none exists
+  factory TodoListModel.startupList() {
+    // return TodoListModel.empty();
+    return TodoListModel.fromJson(jsonDecode("{\"0\": [false, \"hello json\"],\n\"1\": [true, \"wowie zowie\"]}"));
+  }
+
+  TodoListModel.fromJson(Map<String, dynamic> json) 
+    : items = [for(int i = 0; i < json.length; i++) TodoEntry(json[i.toString()][0], json[i.toString()][1])];
 
   void updateContents(int entryIndex, String newText) {
     items[entryIndex].contents = newText;
@@ -32,6 +42,15 @@ class TodoListModel extends ChangeNotifier {
     items.add(TodoEntry(false, ""));
     notifyListeners();
   }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> rval = {};
+    for (int i = 0; i < items.length; i++) {
+      rval.addAll({i.toString(): [items[i].completed, items[i].contents]});
+    }
+    return rval;
+  }
+
 }
 
 class TodoEntry {
