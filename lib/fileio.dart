9f7122d37ae7dbedebf8affa30b994a16c69b5c7
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-02-17 20:40:26 -0500

    Add guards against reading files in web

--- lib/fileio.dart
+++ lib/fileio.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
 class FileIO {
   static void writeFileToDocs(String filename, String text) async {
+    if (kIsWeb) { //! WEB DOES NOT SUPPORT FILES
+      return;
+    }
     final Directory directory = await getApplicationDocumentsDirectory();
     final File file = File("${directory.path}/simple-todo/$filename");
     await file.writeAsString(text); //NOTE - Does not convert to os-specific line terminators (\r\n windows).
@@ -11,6 +15,9 @@ class FileIO {
 
   //Read from application's documents directory. Return empty string if not found.
   static Future<String> readFileFromDocs(String filename) async {
+    if (kIsWeb) { //! WEB DOES NOT SUPPORT FILES
+      return "";
+    }
     String rval = "";
     try {
       final Directory directory = await getApplicationDocumentsDirectory();
@@ -22,7 +29,7 @@ class FileIO {
         File("${directory.path}/simple-todo/$filename").create(recursive: true);
       }
     } on MissingPluginException catch(e) {
-      print("Couldn't read file. File reading is not supported for web platforms!");
+      print("Couldn't read file. File reading may not be supported for this platform.");
     }
     catch (e) {
       print(e.toString());
