13df441ad6d1de8bfae6c5948b50e670a39ec56a
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-02-19 16:58:45 -0500

    Remove empty file

--- lib/components/components_all.dart
+++
@@ -1,8 +0,0 @@
-// library components;
-
-// export "package:simple_todo/components/aboutview.dart";
-// export "package:simple_todo/components/listitemcard.dart";
-// export 'package:simple_todo/components/nothingtosee.dart';
-// export 'package:simple_todo/components/loadingitems.dart';
-// export 'package:simpe'
-
